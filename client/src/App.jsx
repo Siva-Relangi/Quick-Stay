7dd19be5426ecf74198f3d1367e6c6f922d822d3
Siva-Relangi <sivaramarajurelangi@gmail.com>
2025-08-06 19:30:23 +0530

    Home and Rooms Pages completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import { Routes, Route, useLocation } from "react-router-dom";
+import AllRooms from "./pages/AllRooms";
 import Home from "./pages/Home";
+
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/rooms" element={<Rooms />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} /> */}
-      </Routes>
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+        </Routes>
+      </div>
+      {!isOwnerPath && <Footer />}
     </div>
   );
 };
--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,14 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import { roomsDummyData } from "../assets/assets";
 import HotelCard from "./HotelCard";
+import Title from "./Title";
 
 const FeaturedDestination = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
+      <Title
+        title="Featured Destination"
+        subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences."
+        align="left"
+        font="font-playfair"
+      />
+
       <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
         {roomsDummyData.slice(0, 4).map((room, index) => (
           <HotelCard room={room} index={index} key={room._id} />
         ))}
       </div>
+      <button
+        onClick={() => {
+          navigate("/rooms");
+        }}
+        className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer"
+      >
+        View All Destinations
+      </button>
     </div>
   );
 };
