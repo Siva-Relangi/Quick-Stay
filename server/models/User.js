3799324d8b1b6e7a4e84e36b0d624c1aabbd54bb
Siva-Relangi <sivaramarajurelangi@gmail.com>
2025-08-13 23:21:26 +0530

    done with the hotels and room part

--- server/models/User.js
+++ server/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
 
   role: {
     type: String,
-    enum: ["user", "hotel owner"],
+    enum: ["user", "hotelOwner"],
     default: "user",
   },
 
--- server/server.js
+++ server/server.js
@@ -4,8 +4,13 @@ import express from "express";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkwebHooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
 
 connectDB();
+connectCloudinary();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -19,10 +24,15 @@ app.use(clerkMiddleware());
 //API to listen for Clerk webhooks
 app.use("/api/clerk", clerkwebHooks);
 
-
 app.get("/", (req, res) => {
   res.send("API is working");
 });
+
+app.use('/api/user', userRouter);
+
+app.use('/api/hotels', hotelRouter);
+
+app.use('/api/rooms', roomRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
